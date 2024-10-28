5bdb61636953d12024de563e5135698b901befda
--global <--global>
2024-10-28 17:03:54 +0900

    success example

--- face_recognition/Dlib_FaceDetector_Hog.py
+++ face_recognition/Dlib_FaceDetector_Hog.py
@@ -1,11 +1,11 @@
 import dlib
 import cv2
 
-image_path = "face_recognition/face_image/face_img1.JPG"
+image_path = "face_image/face_img1.JPG"
 image = cv2.imread(image_path)
 
 face_detector = dlib.get_frontal_face_detector()
 face_detection = face_detector(image)
 
 for f in face_detection:
-	cv2.rectangle("face_recognition/face_image/face_img1.JPG", (f.left(), f.top()), (f.right(), f.bottom()), (0, 0, 255), 4)
+	cv2.rectangle(image, (f.left(), f.top()), (f.right(), f.bottom()), (0, 0, 255), 4)
