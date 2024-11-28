92819881ccdc4f9079f02d0d6f1277101736f7e1
--global <--global>
2024-11-28 14:44:02 +0900

    commit

--- face_recognition/Dlib_landmark_predictor.py
+++ face_recognition/Dlib_landmark_predictor.py
@@ -1,9 +1,13 @@
 import dlib
+import cv2
+
+image_path = "face_image/face_img1.JPG"
+image = cv2.imread(image_path)
 
 predictor_file = "shape_predictor_68_face_landmarks.dat" #https://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2 에서 다운로드 가능
 face_detector = dlib.get_frontal_face_detector() #얼굴 검출기
 shape_predictor = dlib.shape_predictor(predictor_file) #얼굴 좌표표시
 
-face_detection = face_detector("face_image/face_img1.JPG") #얼굴 검출
+face_detection = face_detector(image) #얼굴 검출
 for f in face_detection:
-	shape = shape_predictor("face_image/face_img1.JPG", f) #68개의 좌표(68*2 사이즈)의 데이터가 저장
+	shape = shape_predictor(image, f) #68개의 좌표(68*2 사이즈)의 데이터가 저장
